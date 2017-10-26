e838df0ab8ee2321b482f2ff8a296d394a8b96d5
Laurent Curau <laurentcurau@gmail.com>
2017-10-26 01:10:18 +0100

    Translate path names

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :users
+  scope(path_names: { new: 'creer', edit: 'editer' }) do
+      resources :users, path: 'utilisateurs'
+  end
 end
