8ec79a3e33d5cde0890340ae45a79f8f5550705b
Laurent Curau <laurentcurau@gmail.com>
2017-10-26 21:21:53 +0100

    Validate user email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, :first_name, :last_name,
             :email, :company, :city,
             presence: true
+
+  validates :email, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should not save user without username" do
+  test "should validate all fields have been filled in" do
     user = User.new
     user.save
     user.valid?
@@ -11,5 +11,14 @@ class UserTest < ActiveSupport::TestCase
     attributes.each do |attribute|
       assert_includes(user.errors[attribute], "doit être rempli(e)")
     end
+  end
+
+  test "email is unique" do
+    user_1 = users(:one)
+    user_2 = User.new(user_1.attributes)
+
+    user_2.valid?
+
+    assert_includes(user_2.errors[:email], "n'est pas disponible")
   end
 end
