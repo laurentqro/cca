181ec1cb787d8bf71646978f385466597625e8bd
Laurent Curau <laurentcurau@gmail.com>
2017-10-26 22:09:33 +0100

    Add uniqueness constraint on email index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016190419) do
+ActiveRecord::Schema.define(version: 20171026210833) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171016190419) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
