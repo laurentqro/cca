d1e78333b2a29df6ad8e3a4fcb6f559d89a8bac2
Laurent Curau <laurentcurau@gmail.com>
2017-12-08 17:38:37 +0000

    User can create folders

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,17 +1,12 @@
 class FoldersController < ApplicationController
 
-  def new
-    project = Project.find(params[:project_id])
-    @folder = project.folders.build
-  end
-
   def show
     @folder = Folder.find(params[:id])
     @document = Document.new
   end
 
   def create
-    project = Project.find(params[:project_id])
+    project = Project.find(params[:folder][:project_id])
     @folder = project.folders.build(folder_params)
 
     if @folder.save
@@ -24,6 +19,6 @@ class FoldersController < ApplicationController
   private
 
   def folder_params
-    params.require(:folder).permit(:name)
+    params.require(:folder).permit(:name, :parent_id)
   end
 end
