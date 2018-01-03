fe649234eb9fb12d035c9693100fdeaa5b01d958
Laurent Curau <laurentcurau@gmail.com>
2018-01-03 18:37:21 +0000

    Add sessions/registrations permissions

--- test/models/permission_test.rb
+++ test/models/permission_test.rb
@@ -3,9 +3,17 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test 'all users permissions' do
     permission = Permission.new(nil)
-    assert permission.allow_action?(:sessions, :new)
-    assert permission.allow_action?(:sessions, :create)
-    assert permission.allow_action?(:sessions, :destroy)
+    assert permission.allow_action?('devise/sessions', :new)
+    assert permission.allow_action?('devise/sessions', :create)
+  end
+
+  test 'any logged in user permissions' do
+    user = users(:one)
+    permission = Permission.new(user)
+
+    assert permission.allow_action?('devise/registrations', :new)
+    assert permission.allow_action?('devise/registrations', :create)
+    assert permission.allow_action?('devise/sessions', :destroy)
   end
 
   test 'partner permissions' do
