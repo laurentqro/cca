91748b0127755be1015a5caadd090951b6fe89ba
Laurent Curau <laurentcurau@gmail.com>
2018-01-03 18:33:39 +0000

    Update welcome email mailer class

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @login_url = new_session_url
+    @login_url = new_user_session_url
     email_with_name = %("#{@user.full_name}" <#{@user.email}>)
 
     mail(
