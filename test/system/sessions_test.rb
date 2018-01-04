c4d475f3bff5b9a0e79e3799fccdaadd976d8cc4
Laurent Curau <laurentcurau@gmail.com>
2018-01-04 20:37:16 +0000

    logged in user who visits the log in page is taken to the homepage

--- test/system/sessions_test.rb
+++ test/system/sessions_test.rb
@@ -35,6 +35,19 @@ class SessionsTest < ApplicationSystemTestCase
     assert_text 'Identifiant ou mot de passe incorrect'
   end
 
+  test "logged in user who visits the log in page is taken to the homepage" do
+    visit new_user_session_url
+
+    fill_in 'user[email]', with: users(:one).email
+    fill_in 'user[password]', with: 'password'
+
+    click_button 'Se connecter'
+
+    visit new_user_session_url
+
+    assert_current_path root_path
+  end
+
   test "user directed to the page he intended to visit before being asked to sign in" do
     visit projects_path
     assert_current_path new_user_session_path
