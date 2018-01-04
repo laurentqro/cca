198e7703cb7349b153832b0f5c47355be9843d63
Laurent Curau <laurentcurau@gmail.com>
2018-01-04 20:28:06 +0000

    Fix devise paths in user permission test

--- test/models/permission_test.rb
+++ test/models/permission_test.rb
@@ -3,17 +3,17 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test 'all users permissions' do
     permission = Permission.new(nil)
-    assert permission.allow_action?('devise/sessions', :new)
-    assert permission.allow_action?('devise/sessions', :create)
+    assert permission.allow_action?('users/sessions', :new)
+    assert permission.allow_action?('users/sessions', :create)
   end
 
   test 'any logged in user permissions' do
     user = users(:one)
     permission = Permission.new(user)
 
-    assert permission.allow_action?('devise/registrations', :new)
-    assert permission.allow_action?('devise/registrations', :create)
-    assert permission.allow_action?('devise/sessions', :destroy)
+    assert permission.allow_action?('users/registrations', :new)
+    assert permission.allow_action?('users/registrations', :create)
+    assert permission.allow_action?('users/sessions', :destroy)
   end
 
   test 'partner permissions' do
