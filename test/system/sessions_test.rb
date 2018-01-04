aadbb577650827cd99c61c1f64225f8e240332fd
Laurent Curau <laurentcurau@gmail.com>
2018-01-04 20:35:16 +0000

    user directed to the page he intended to visit before being asked to sign in

--- test/system/sessions_test.rb
+++ test/system/sessions_test.rb
@@ -34,4 +34,16 @@ class SessionsTest < ApplicationSystemTestCase
 
     assert_text 'Identifiant ou mot de passe incorrect'
   end
+
+  test "user directed to the page he intended to visit before being asked to sign in" do
+    visit projects_path
+    assert_current_path new_user_session_path
+
+    fill_in 'user[email]', with: users(:one).email
+    fill_in 'user[password]', with: 'password'
+
+    click_button 'Se connecter'
+
+    assert_current_path projects_path
+  end
 end
