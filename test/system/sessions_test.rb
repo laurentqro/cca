100ee1fd3f7a750cb915de587104d5f2b55e6ad6
Laurent Curau <laurentcurau@gmail.com>
2018-01-04 20:49:15 +0000

    Remove ability to log in with username for now

--- test/system/sessions_test.rb
+++ test/system/sessions_test.rb
@@ -2,17 +2,6 @@ require "application_system_test_case"
 require 'test_helper'
 
 class SessionsTest < ApplicationSystemTestCase
-  test 'logging in with username' do
-    visit new_user_session_url
-
-    fill_in 'user[email]', with: users(:one).username
-    fill_in 'user[password]', with: 'password'
-
-    click_button 'Se connecter'
-
-    assert_text 'Vous êtes connecté'
-  end
-
   test 'logging in with email' do
     visit new_user_session_url
 
