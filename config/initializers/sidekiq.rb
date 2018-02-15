8e3aa710ec07ab488b5498921a5c907ddef38cb4
Laurent Curau <laurentcurau@gmail.com>
2018-02-15 20:25:44 +0000

    Configure Redis for production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,10 @@
+redis_config = Rails.application.config_for(:redis)
+redis_config.symbolize_keys!
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379' }
+  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379' }
+  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
 end
