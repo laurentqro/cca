b7e7de960c1a7a51185de4dc7c461addb662d7e3
Laurent Curau <laurentcurau@gmail.com>
2018-02-17 00:46:51 +0000

    Test: folder name must be filled in

--- test/models/folder_test.rb
+++ test/models/folder_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class FolderTest < ActiveSupport::TestCase
-  test "folder name should be unique in the scope of its parent folder (i.e. siblings have unique names)" do
+  test  'folder name must be present' do
+    folder = Folder.new
+    folder.valid?
+    assert_includes(folder.errors[:name], "doit être rempli(e)")
+  end
+
+  test 'folder name should be unique in the scope of its parent folder (i.e. siblings have unique names)' do
     parent_folder = folders(:folder_one)
     sibling_folder = parent_folder.siblings.build(name: parent_folder.name)
 
