ae976f70fa78b998ef76d28ed7d8633b2af3de11
Laurent Curau <laurentcurau@gmail.com>
2018-02-17 00:50:13 +0000

    Test: folder must be associated to a project

--- test/models/folder_test.rb
+++ test/models/folder_test.rb
@@ -10,9 +10,13 @@ class FolderTest < ActiveSupport::TestCase
   test 'folder name should be unique in the scope of its parent folder (i.e. siblings have unique names)' do
     parent_folder = folders(:folder_one)
     sibling_folder = parent_folder.siblings.build(name: parent_folder.name)
-
     sibling_folder.valid?
-
     assert_includes(sibling_folder.errors[:name], "indisponible. Merci d'en choisir un autre.")
+  end
+
+  test 'folder must be associated to a project' do
+    folder = Folder.new
+    folder.valid?
+    assert_includes(folder.errors[:project], "doit exister")
   end
 end
