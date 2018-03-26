5213198a3ebbbeb1d631bf765be97a84994f569f
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2018-03-26 16:06:22 +0100

    Set up new CSP initializer

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,14 +1,14 @@
-Rails.application.config.content_security_policy do |p|
-  # allow everything from the current hostname by default (only secured)
-  p.default_src :self, :https
-  # allow loading fonts and images from data-uri
-  p.font_src    :self, :https, :data
-  # don't forget to add your cloud hostname for ActiveStorage assets
-  p.img_src     :self, :https, :data, "cloudfront.example.com"
-  # disallow <object> tags (Good-bye Flash!)
-  p.object_src  :none
-  # allow inline <style> (remove :unsafe_inline if you don't want it)
-  p.style_src   :self, :https, :unsafe_inline
-  # allow webpack-dev-server host as allowed origin for connect-src
-  p.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data
+  policy.img_src     :self, :https, :data
+  policy.object_src  :none
+  policy.script_src  :self, :https
+  policy.style_src   :self, :https
+
+  # Specify URI for violation reports
+  # policy.report_uri "/csp-violation-report-endpoint"
 end
+
+# If you are using UJS then enable automatic nonce generation
+Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
