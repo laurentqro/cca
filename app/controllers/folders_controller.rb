4f6dfaa3477cd9aa82a5c20c1dc33ed4e80065c6
Laurent Curau <laurentcurau@gmail.com>
2018-03-28 23:17:19 +0100

    Fix access to folders/documents

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -35,6 +35,6 @@ class FoldersController < ApplicationController
   end
 
   def current_resource
-    @current_resource ||= Folder.find(params[:id]) if params[:id]
+    @current_resource ||= Project.find(params[:project_id]) if params[:project_id]
   end
 end
