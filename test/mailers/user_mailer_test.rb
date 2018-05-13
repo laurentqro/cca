a05f8a75e8381412b3c6fafd389687d2cc550be1
Laurent Curau <laurentcurau@gmail.com>
2018-05-13 21:40:45 +0100

    Test new document mailer

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -18,4 +18,38 @@ class UserMailerTest < ActionMailer::TestCase
     assert_includes email.html_part.body.to_s, user.first_name
     assert_includes email.html_part.body.to_s, "#{Rails.application.secrets[:host]}/auth/connexion"
   end
+
+  test "new_document" do
+    user = users(:one)
+    folder = folders(:folder_one)
+
+    s3_url = %r{https://#{ENV['AWS_S3_BUCKET']}.s3.#{ENV['AWS_S3_REGION']}.amazonaws.com}
+    stub_request(:put, s3_url).to_return(body: '', status: 200)
+
+    document = Document.create(
+      file: File.open('test/fixtures/files/pdf-sample.pdf'),
+      folder: folder,
+      user: user
+    )
+
+    activity = user.activities.create(action: "create",
+                                     trackable: document,
+                                     project:   folder.project,
+                                     folder:    folder)
+
+    folder.project.subscribers << users(:two)
+
+    email = UserMailer.new_document(activity)
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal 'Cabinet CCA admin@archicc.com', email.from
+    assert_equal [users(:two).email], email.to
+    assert_equal "Opération #{activity.project.name} - nouveau document", email.subject
+    assert_includes email.html_part.body.to_s, "#{activity.user.full_name}"
+    assert_includes email.html_part.body.to_s, "#{activity.user.company.name}"
+    assert_includes email.html_part.body.to_s, "#{activity.trackable.file.original_filename}"
+  end
 end
