e705f577c92cd76df0953316b6ad9608c7eebf40
Laurent Curau <laurentcurau@gmail.com>
2018-08-09 18:34:07 +0100

    Set default url options for dev environment to localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
 
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+
   config.active_job.queue_adapter = :sidekiq
 
   # Print deprecation notices to the Rails logger.
