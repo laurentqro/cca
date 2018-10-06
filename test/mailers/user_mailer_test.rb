88ba14cbafe06735dcdc9e3e07c0ebb814f0254f
Laurent Curau <laurentcurau@gmail.com>
2018-10-06 22:54:17 +0100

    Update file upload mailer test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -27,10 +27,14 @@ class UserMailerTest < ActionMailer::TestCase
     stub_request(:put, s3_url).to_return(body: '', status: 200)
 
     document = Document.create(
-      file: File.open('test/fixtures/files/pdf-sample.pdf'),
       folder: folder,
       user: user
     )
+
+    document.file.attach(io: File.open('test/fixtures/files/pdf-sample.pdf'),
+                         filename: 'pdf-sample.pdf',
+                         content_type: 'application/pdf')
+
 
     activity = user.activities.create(action: "create",
                                      trackable: document,
