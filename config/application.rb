903fefa6abd741418e7dd738dd3534989ba33db9
Laurent Curau <laurentcurau@gmail.com>
2018-10-18 23:51:15 +0100

    Reconfigure default behaviour of generators

--- config/application.rb
+++ config/application.rb
@@ -2,20 +2,21 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Cca
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :fr
     config.action_mailer.default_url_options = { host: ENV['HOST'] }
     config.paths.add "app/serializers", eager_load: true
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.channel         assets: false
+    end
   end
 end
